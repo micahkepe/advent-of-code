c79450aeef9e84774e92a18f612eeb52d40169dc
micahkepe <micahkepe@gmail.com>
2025-12-07 21:02:45 -0700

    feat(aoc2025): day 1 part 2

--- 2025/src/bin/day_01.rs
+++ 2025/src/bin/day_01.rs
@@ -17,21 +17,50 @@ fn position_change(input: &str) -> anyhow::Result<isize> {
 
 fn main() -> anyhow::Result<()> {
     // dial starts at 50
-    let mut position: usize = 50;
+    let mut position: isize = 50;
     let f = std::fs::File::open("./data/day-01-input.txt")?;
     let f = BufReader::new(f);
-    let mut password = 0;
+    let mut password_1 = 0;
+    let mut password_2 = 0;
+
     for line in f.lines() {
         let line = line.expect("invalid line");
         let delta = position_change(&line)?;
-        position = (position as isize + delta).rem_euclid(100) as usize;
+        let old_pos = position;
+        let new_pos = (position + delta).rem_euclid(100);
 
-        if position == 0 {
-            password += 1;
+        // Part 1
+        if new_pos == 0 {
+            password_1 += 1;
         }
+
+        // Part 2
+        let zero_crossings = if delta == 0 {
+            0
+        } else {
+            let steps = delta.abs();
+            let mut count = 0;
+            for i in 1..=steps {
+                let pos = if delta > 0 {
+                    (old_pos + i).rem_euclid(100)
+                } else {
+                    (old_pos - i).rem_euclid(100)
+                };
+
+                if pos == 0 {
+                    count += 1
+                }
+            }
+
+            count
+        };
+        password_2 += zero_crossings;
+
+        position = new_pos
     }
 
-    println!("Password: {}", password);
+    println!("Part 1 password: {}", password_1);
+    println!("Part 2 password: {}", password_2);
 
     Ok(())
 }
