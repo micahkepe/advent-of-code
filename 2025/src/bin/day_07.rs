0f016e0a346817afaa41b351e75135b20f06b184
micahkepe <micahkepe@gmail.com>
2025-12-12 15:05:44 -0600

    feat(aoc2025): day 7 part 2

--- 2025/src/bin/day_07.rs
+++ 2025/src/bin/day_07.rs
@@ -1,18 +1,24 @@
 use std::collections::HashSet;
 
-fn compute_total_beam_splits(contents: &str) -> anyhow::Result<usize> {
+fn compute_total_beam_splits(contents: &str) -> anyhow::Result<(usize, usize)> {
     let contents = contents.trim();
     let mut curr_beam_indices: HashSet<usize> = HashSet::new();
-    curr_beam_indices.insert(
-        contents
-            .lines()
-            .next()
-            .unwrap()
-            .find('S')
-            .expect("no 'S' on first line"),
-    );
+    let starting_idx = contents
+        .lines()
+        .next()
+        .unwrap()
+        .find('S')
+        .expect("no 'S' on first line");
+
+    curr_beam_indices.insert(starting_idx);
 
     let mut total_splits = 0;
+
+    // beam_counts[pos] -> number of timelines with beam at that position
+    let cols = contents.lines().next().unwrap().chars().count();
+    let mut beam_counts = vec![0; cols];
+    beam_counts[starting_idx] = 1;
+
     for line in contents.lines().skip(1) {
         let splitter_indices: HashSet<usize> =
             line.match_indices("^").map(|(idx, _)| idx).collect();
@@ -43,6 +49,11 @@ fn compute_total_beam_splits(contents: &str) -> anyhow::Result<usize> {
             total_splits += 1;
             new_active.insert(splitter_idx - 1);
             new_active.insert(splitter_idx + 1);
+
+            let count = beam_counts[splitter_idx];
+            beam_counts[splitter_idx] = 0;
+            beam_counts[splitter_idx - 1] += count;
+            beam_counts[splitter_idx + 1] += count;
         }
 
         // update active indices
@@ -52,15 +63,20 @@ fn compute_total_beam_splits(contents: &str) -> anyhow::Result<usize> {
         println!("beams: {:?}", curr_beam_indices)
     }
 
-    Ok(total_splits)
+    let total_combinations = beam_counts.iter().sum();
+
+    Ok((total_splits, total_combinations))
 }
 
 fn main() -> anyhow::Result<()> {
     let contents = std::fs::read_to_string("./data/day-07-input.txt")?;
 
     /* Part 1 */
-    let total_beam_splits = compute_total_beam_splits(&contents)?;
+    let (total_beam_splits, total_beam_combos) = compute_total_beam_splits(&contents)?;
     println!("Part 1: {}", total_beam_splits);
+
+    /* Part 2 */
+    println!("Part 2: {}", total_beam_combos);
 
     Ok(())
 }
@@ -89,6 +105,29 @@ mod test {
 .^.^.^.^.^...^.
 ...............
             ";
-        assert_eq!(compute_total_beam_splits(input).unwrap(), 21);
+        assert_eq!(compute_total_beam_splits(input).unwrap().0, 21);
+    }
+
+    #[test]
+    fn test_part2_example() {
+        let input = "
+.......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+            ";
+        assert_eq!(compute_total_beam_splits(input).unwrap().1, 40);
     }
 }
