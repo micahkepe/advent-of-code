e96a551cfd237e8de9dc8d8f7221f1d5bd4e36d6
Micah Kepe <micahkepe@gmail.com>
2026-06-21 20:35:07 -0700

    feat(2024): day 3 part 2

--- 2024/src/bin/day_03.rs
+++ 2024/src/bin/day_03.rs
@@ -1,33 +1,33 @@
 use regex::Regex;
 use std::error::Error;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-
-// only accept mul(X,Y), where X, Y are 1-3 digit numbers without surrounding
-// whitespace (or trailing zeroes?)
-// return the summation of the valid mul instructions
+use std::fs;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let file =
-        File::open("data/day_03_input.txt").expect("Input file doesn't exist");
-    let reader = BufReader::new(file);
+    let input = fs::read_to_string("data/day_03_input.txt")?;
 
-    let re = Regex::new(r"mul\(([1-9][0-9]{0,2}),([1-9][0-9]{0,2})\)")?;
-    let mut total_sum = 0;
+    let re = Regex::new(r"mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)")?;
+    let mut part1_sum: i64 = 0;
+    let mut part2_sum: i64 = 0;
+    let mut enabled = true;
 
-    for line in reader.lines() {
-        let line = line?;
-
-        for cap in re.captures_iter(&line) {
-            let x: i32 = cap[1].parse()?;
-            let y: i32 = cap[2].parse()?;
-            total_sum += x * y;
+    for cap in re.captures_iter(&input) {
+        let token = cap.get(0).unwrap().as_str();
+        if token == "do()" {
+            enabled = true;
+        } else if token == "don't()" {
+            enabled = false;
+        } else {
+            let x: i64 = cap[1].parse()?;
+            let y: i64 = cap[2].parse()?;
+            part1_sum += x * y;
+            if enabled {
+                part2_sum += x * y;
+            }
         }
     }
 
-    // TODO: part 2 with setting enable/disabled sections
-
-    println!("Total sum of muls: {}", total_sum);
+    println!("PART 1: {}", part1_sum);
+    println!("PART 2: {}", part2_sum);
 
     Ok(())
 }
